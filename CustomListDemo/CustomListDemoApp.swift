0c6846357ef5387a33ea2d395cce9e8c165232a6
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-02-14 09:37:20 +0600

    Initial Commit

---
+++ CustomListDemo/CustomListDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomListDemoApp.swift
+//  CustomListDemo
+//
+//  Created by Joynal Abedin on 14/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomListDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
